a8c8c9aeffe68a8ad261bba6a43478f5651ad8a3
Mark Jordan <mark.jordan@red-gate.com>
2024-01-26 16:19:31 +0000

    add todos to the list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+  import type { SvelteComponent } from 'svelte';
   import { writable } from 'svelte/store';
   let todos_data = [
     { text: 'Learn Svelte', done: false },
     { text: 'Build something awesome', done: false }
   ];
   export let todos = writable(todos_data);
+  let new_todo_name: HTMLInputElement;
 </script>
 
 <h1>event-sourced svelte ff14 todo</h1>
@@ -16,3 +18,14 @@
     </li>
   {/each}
 </ul>
+
+<input type="text" placeholder="add new todo" bind:this={new_todo_name} />
+<button
+  on:click={() => {
+    todos.update((todos) => {
+      todos.push({ text: new_todo_name.value, done: false });
+      return todos;
+    });
+    new_todo_name.value = '';
+  }}>add</button
+>
