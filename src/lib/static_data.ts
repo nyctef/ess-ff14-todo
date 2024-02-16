109ad11582edf6496c8f139a4b35fa5f868562f2
Mark Jordan <Mark.Jordan@red-gate.com>
2024-02-16 15:37:42 +0000

    add island sanctuary reset at 8am

--- src/lib/static_data.ts
+++ src/lib/static_data.ts
@@ -5,7 +5,8 @@ export const resets: Reset[] = [
   { name: 'Weekly reset', interval: 'weekly', hourOffset: 24 + 8 },
   { name: 'Duty reset', interval: 'daily', hourOffset: 15 },
   { name: 'GC Supply reset', interval: 'daily', hourOffset: 20 },
-  { name: 'Jumbo Cactpot reset', interval: 'weekly', hourOffset: 5 * 24 + 20 }
+  { name: 'Jumbo Cactpot reset', interval: 'weekly', hourOffset: 5 * 24 + 20 },
+  { name: 'Island Sanctuary reset', interval: 'daily', hourOffset: 8 }
 ];
 
 export function get_reset_by_name(name: string): Reset {
