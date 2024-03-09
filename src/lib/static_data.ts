c862f41f634818183dc56aa61b6ce2695dab0adc
Nyctef <nyctef@nyctef.com>
2024-03-09 19:05:07 +0000

    push jumbo cactpot an hour earlier
    seems to be at 7pm in GMT rather than 8pm

--- src/lib/static_data.ts
+++ src/lib/static_data.ts
@@ -5,7 +5,7 @@ export const resets: Reset[] = [
   { name: 'Weekly reset', interval: 'weekly', hourOffset: 24 + 8 },
   { name: 'Duty reset', interval: 'daily', hourOffset: 15 },
   { name: 'GC Supply reset', interval: 'daily', hourOffset: 20 },
-  { name: 'Jumbo Cactpot reset', interval: 'weekly', hourOffset: 5 * 24 + 20 },
+  { name: 'Jumbo Cactpot reset', interval: 'weekly', hourOffset: 5 * 24 + 19 },
   { name: 'Island Sanctuary reset', interval: 'daily', hourOffset: 8 }
 ];
 
