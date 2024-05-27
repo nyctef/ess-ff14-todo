8c88a51de51d40fe1eef9c9ae7a52d97245d5052
Nyctef <nyctef@nyctef.com>
2024-05-27 17:31:14 +0100

    wip: a broken signup page that just about manages to work once

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -11,7 +11,25 @@ if (!connectionString) {
   throw new Error('FF14_TODO_PG_CONNECTION_STRING not set');
 }
 
-export const client = new pg.Client(connectionString);
+export const client = new pg.Pool({
+  connectionString,
+  statement_timeout: 2000,
+  query_timeout: 2000,
+  //lock_timeout: 2000,
+  application_name: 'ess-ff14-todo'
+});
+client.on('error', (err) => {
+  console.error('Unexpected error on idle client', err);
+});
+// client.on('end', () => {
+//   console.log('client has disconnected');
+// });
+// client.on('notification', (msg) => {
+//   console.log('notification:', msg);
+// });
+// client.on('notice', (msg) => {
+//   console.log('notice:', msg);
+// });
 
 const adapter = new NodePostgresAdapter(client, {
   user: 'auth_user',
