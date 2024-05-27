0fad3e1f44d90adb867e962a8923c906ca7afa45
Nyctef <nyctef@nyctef.com>
2024-05-27 19:57:47 +0100

    wip: hacking stuff to try and work around build errors

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,28 @@
+import { building } from '$app/environment';
 import { EventSourcedApi } from '$lib/event_sourced_api';
+import { InMemoryEventStorage } from '$lib/in_memory_event_storage';
 import { PostgresEventStorage } from '$lib/postgres_event_storage';
 import { lucia } from '$lib/server/auth';
 import type { Handle } from '@sveltejs/kit';
+import pg from 'pg';
 
-const storage = await PostgresEventStorage.create_from_env();
-const api = await EventSourcedApi.create(storage);
+const connectionString = process.env.FF14_TODO_PG_CONNECTION_STRING;
+// if (!connectionString) {
+//   throw new Error(`FF14_TODO_PG_CONNECTION_STRING not set (asdf)`);
+// }
+export const client = new pg.Pool({
+  connectionString,
+  statement_timeout: 2000,
+  query_timeout: 2000,
+  //lock_timeout: 2000,
+  application_name: 'ess-ff14-todo'
+});
+client.on('error', (err: unknown) => {
+  console.error('Unexpected error on idle client', err);
+});
+
+const storage = new PostgresEventStorage(client);
+const api = building ? new InMemoryEventStorage() : await EventSourcedApi.create(storage);
 
 // based on https://lucia-auth.com/getting-started/sveltekit
 
--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -1,26 +1,12 @@
 import { Lucia } from 'lucia';
-import { dev } from '$app/environment';
+import { building, dev } from '$app/environment';
 import { NodePostgresAdapter } from '@lucia-auth/adapter-postgresql';
-import pg from 'pg';
+import { client } from '../../hooks.server';
 
 // file based on https://lucia-auth.com/getting-started/sveltekit
 
 // TODO: dedupe
-const connectionString = process.env.FF14_TODO_PG_CONNECTION_STRING;
-if (!connectionString) {
-  throw new Error('FF14_TODO_PG_CONNECTION_STRING not set');
-}
 
-export const client = new pg.Pool({
-  connectionString,
-  statement_timeout: 2000,
-  query_timeout: 2000,
-  //lock_timeout: 2000,
-  application_name: 'ess-ff14-todo'
-});
-client.on('error', (err) => {
-  console.error('Unexpected error on idle client', err);
-});
 // client.on('end', () => {
 //   console.log('client has disconnected');
 // });
@@ -30,6 +16,7 @@ client.on('error', (err) => {
 // client.on('notice', (msg) => {
 //   console.log('notice:', msg);
 // });
+export { client };
 
 const adapter = new NodePostgresAdapter(client, {
   user: 'users',
